7e4eb06d8fa4f33cc83afced3d9527ce0a464bd3
Scalza10 <scalza15@gmail.com>
2025-03-01 15:03:49 -0500

    adding api Keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,15 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
+const apiUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const apiKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!apiUrl || !apiKey) {
+  throw new Error('Missing Supabase URL or key');
+}
+
 // Create the clients
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(apiUrl, apiKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
